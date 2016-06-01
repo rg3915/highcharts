750ce7c85c10295dd45e1dec9d85b02c50af5d48
rg3915 <rg3915@yahoo.com.br>
2016-06-01 17:26:55 -0300

    index.html

--- highcharts/core/urls.py
+++ highcharts/core/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from highcharts.core.graphics import dollar_json, euro_json, product_json
-from highcharts.core.views import dollar_graphic, euro_graphic, product_graphic
+from highcharts.core import views as v
 
 urlpatterns = [
-    url(r'^dollar-graphic/$', dollar_graphic, name='dollar-graphic'),
-    url(r'^euro-graphic/$', euro_graphic, name='euro-graphic'),
-    url(r'^product-graphic/$', product_graphic, name='product-graphic'),
+    url(r'^$', v.home, name='home'),
+    url(r'^dollar-graphic/$', v.dollar_graphic, name='dollar-graphic'),
+    url(r'^euro-graphic/$', v.euro_graphic, name='euro-graphic'),
+    url(r'^product-graphic/$', v.product_graphic, name='product-graphic'),
     url(r'^dollar_json/$', dollar_json),
     url(r'^euro_json/$', euro_json),
     url(r'^product_json/$', product_json),
--- highcharts/core/views.py
+++ highcharts/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
+def home(request):
+    return render(request, 'index.html')
+
+
 def dollar_graphic(request):
     return render(request, 'dollar_graphic.html')
 
