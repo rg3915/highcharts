0bf4a847967ebbbc3f311c0cf86f3ac5cdbf7cb0
rg3915 <rg3915@yahoo.com.br>
2016-06-01 14:02:03 -0300

    setup

--- setup.sh
+++ setup.sh
@@ -10,13 +10,14 @@ echo "${green}>>> Creating virtualenv${reset}"
 python -m venv .venv
 source .venv/bin/activate
 
-cd highcharts
-
 echo "${green}>>> Installing the Django${reset}"
 pip install -r requirements.txt
+cp contrib/env-sample .env
 python manage.py makemigrations core
 python manage.py migrate
 python manage.py shell < highcharts/shell/shell_dollar.py
 python manage.py shell < highcharts/shell/shell_euro.py
+python manage.py shell < highcharts/shell/shell_category.py
+python manage.py shell < highcharts/shell/shell_product.py
 python manage.py runserver
 echo "${green}>>> Done${reset}"
