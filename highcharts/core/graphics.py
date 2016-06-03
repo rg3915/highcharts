8297e301023ecd758ffa9a5c75e9e796627ee2f6
Eduardo Cuducos <cuducos@gmail.com>
2016-06-03 15:55:50 -0300

    Test dollar_json view

--- highcharts/core/graphics.py
+++ highcharts/core/graphics.py
@@ -1,15 +1,14 @@
 import json
 from django.db.models import Count
 from django.core.serializers.json import DjangoJSONEncoder
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Dollar, Euro, Product
 
 
 def dollar_json(request):
     data = Dollar.objects.values('date', 'value')
     lista = [{'dia': i['date'], 'valor': float(i['value'])} for i in data]
-    resp = json.dumps(lista, cls=DjangoJSONEncoder)
-    return HttpResponse(resp)
+    return JsonResponse({'dollar': lista})
 
 
 def euro_json(request):
